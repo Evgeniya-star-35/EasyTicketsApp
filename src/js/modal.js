bbd336c6bd88b218fe56937e99fce06e68250579
tanyamelnyk19 <79224689+tanyamelnyk19@users.noreply.github.com>
2021-09-17 00:58:47 +0300

    add modalHbs

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,6 @@
 import { refs } from './refs';
+import modalHbs from '../templates/galleryModalCard.hbs';
+import NewsApiService from './fetchEvents';
 
 // function onEventClick(e) {
 //   e.preventDefault();
@@ -23,8 +25,21 @@ export function onEventClick(e) {
 function modalOpen(e) {
   refs.backdrop.classList.remove('is-hidden');
 
+  const fetchCardInfo = new NewsApiService();
+fetchCardInfo.fetchEvents()
+.then(events => console.log(events))
+// .then(events => events.map((el, index, array) => {
+//   renderModalCard(el)
+// }));
+.then(events => renderModalCard(events));
+
   window.addEventListener('keyup', modalCloseESC);
   window.addEventListener('click', modalCloseOverlay);
+}
+
+function renderModalCard(events) {
+  const markup = modalHbs(events);
+  refs.modal.innerHTML = markup;
 }
 
 // модальное окно закрытие
@@ -32,6 +47,16 @@ refs.closeButton.addEventListener('click', modalClose);
 
 function modalClose(e) {
   refs.backdrop.classList.add('is-hidden');
+
+  // refs.modal.innerHTML = `<div class="backdrop is-hidden">
+  // <div class="modal">
+  // <button class="modal__close-button">
+  //         <svg class="modal__icon-close" width="17" height="17">
+  //             <use href="./images/sprite.svg#icon-close"></use>
+  //         </svg>
+  //     </button>
+  //     </div>
+  //     </div>`;
 
   window.removeEventListener('keyup', modalCloseESC);
   window.removeEventListener('click', modalCloseOverlay);
@@ -55,3 +80,4 @@ function modalCloseOverlay(e) {
   }
   modalClose(e);
 }
+
